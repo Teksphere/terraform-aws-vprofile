aaeeda473ce8f1afe3cd0755a5379678cca97be0
ibk.teksphere <ibk.teksphere@gmail.com>
2024-06-07 15:21:39 +0100

    host

--- bastion-host.tf
+++ bastion-host.tf
@@ -24,6 +24,7 @@ resource "aws_instance" "vprofile-bastion" {
   }
 
   connection {
+    type        = "ssh"
     user        = var.USERNAME
     private_key = file(var.PRIV_KEY_PATH)
     host        = self.public_ip
