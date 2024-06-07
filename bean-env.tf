91b9099411cdf0109c63a3b266c1cf3ff5f1ddef
ibk.teksphere <ibk.teksphere@gmail.com>
2024-06-07 13:55:53 +0100

    service role

--- bean-env.tf
+++ bean-env.tf
@@ -3,6 +3,13 @@ resource "aws_elastic_beanstalk_environment" "teksphere-bean-prod" {
   application         = aws_elastic_beanstalk_application.teksphere-prod.name
   solution_stack_name = "64bit Amazon Linux 2023 v5.1.7 running Tomcat 9 Corretto 11"
   cname_prefix        = "teksphere-bean-prod-domain"
+  service_role        = aws_iam_role.beanstalk-fullaccess.arn
+}
+
+resource "aws_iam_role" "beanstalk-fullaccess" {
+  name = "beanstalk-fullaccess"
+  }
+
   setting {
     name      = "VPCId"
     namespace = "aws:ec2:vpc"
