8a8f796d927c70b27d74d3710be94a53f7f74b10
ibk.teksphere <ibk.teksphere@gmail.com>
2024-06-07 12:47:50 +0100

    module correction

--- bean-app.tf
+++ bean-app.tf
@@ -1,3 +1,3 @@
-resource "aws_elastic_beanstalk_application" "teksphere-prod" {
-  name = "teksphere-prod"
+resource "aws_elastic_beanstalk_application" "vprofile-prod" {
+  name = "vprofile-prod"
 }
