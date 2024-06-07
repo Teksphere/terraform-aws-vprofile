abd3add9941f919ad13f73413a3f28c0739c54de
ibk.teksphere <ibk.teksphere@gmail.com>
2024-06-07 12:57:40 +0100

    teksphere

--- bean-env.tf
+++ bean-env.tf
@@ -1,8 +1,8 @@
-resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
-  name                = "vprofile-bean-prod"
-  application         = aws_elastic_beanstalk_application.vprofile-prod.name
+resource "aws_elastic_beanstalk_environment" "teksphere-bean-prod" {
+  name                = "teksphere-bean-prod"
+  application         = aws_elastic_beanstalk_application.teksphere-prod.name
   solution_stack_name = "64bit Amazon Linux 2023 v5.1.7 running Tomcat 9 Corretto 11"
-  cname_prefix        = "vprofile-bean-prod-domain"
+  cname_prefix        = "teksphere-bean-prod-domain"
   setting {
     name      = "VPCId"
     namespace = "aws:ec2:vpc"
